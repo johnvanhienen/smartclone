810dfbf9bd3ab41bffdf85e35d6eb07ca8111c63
John van Hienen <jvanhienen@gmail.com>
2022-05-30 22:45:44 +0200

    Basic url parsing

--- main.go
+++ main.go
@@ -2,26 +2,55 @@ package main
 
 import (
     "fmt"
+    "os"
     "strings"
 )
 
+type Repo struct {
+    source string
+    path   string
+}
+
+type context struct {
+    defaultPath string
+}
+
+func (c *context) fillDefaults() {
+    c.defaultPath = fmt.Sprintf("%s/git", os.Getenv("HOME"))
+}
+
 func main() {
-    fmt.Println("vim-go")
+    ctx := context{}
+    ctx.fillDefaults()
+    fmt.Println(scrubUrl("https://gitlab.com/jvanhienen/dotfiles.git"))
+    fmt.Println(scrubUrl("git@gitlab.com:jvanhienen/dotfiles.git"))
 }
 
-func scrubUrl(url string) (cleanUrl string) {
+func scrubUrl(url string) (repo Repo) {
     if strings.HasPrefix(url, "git@") {
-        cleanUrl = scrubSshUrl(url)
+        repo = scrubSshUrl(url)
+    } else if strings.HasPrefix(url, "https://") {
+        repo = scrubHttpsUrl(url)
+    } else {
+        fmt.Println("Please provide a url that starts with 'git@' or 'https://'")
+        os.Exit(1)
     }
-
-    // cleanUrl = "jvanhienen/dotfile"
-    return cleanUrl
+    return repo
 }
 
-func scrubSshUrl(url string) string {
-    splitByColon := strings.SplitAfter(url, ":")
-    noPrefixUrl := splitByColon[len(splitByColon)-1]
-    noSuffixUrl := strings.TrimSuffix(noPrefixUrl, ".git")
+func scrubSshUrl(url string) (r Repo) {
+    splitUrl := strings.Split(url, ":")
+    r.source = strings.TrimPrefix(splitUrl[0], "git@")
+    r.path = strings.TrimSuffix(splitUrl[1], ".git")
+    return r
+}
 
-    return noSuffixUrl
+func scrubHttpsUrl(url string) (r Repo) {
+    r.source = strings.Split(url, "/")[2]
+
+    prefix := fmt.Sprintf("https://%s/", r.source)
+    pathNoPrefix := strings.TrimPrefix(url, prefix)
+    r.path = strings.TrimSuffix(pathNoPrefix, ".git")
+    
+    return r
 }
--- main_test.go
+++ main_test.go
@@ -6,14 +6,30 @@ import (
 
 func TestScrubUrl(t *testing.T) {
     t.Run("test ssh git url", func(t *testing.T) {
-        url := "git@gitlab.com:jvanhienen/dotfiles.git"
+        repoStructure := Repo{
+            source: "gitlab.com",
+            path:   "user/project",
+        }
+        url := "git@gitlab.com:user/project.git"
         got := scrubUrl(url)
-        want := "jvanhienen/dotfiles"
+        want := repoStructure
 
         if got != want {
             t.Errorf("got %q want %q", got, want)
         }
 
     })
-    // https://gitlab.com/jvanhienen/dotfiles.git
+    t.Run("test https git url", func(t *testing.T) {
+        repoStructure := Repo{
+            source: "gitlab.com",
+            path:   "user/project",
+        }
+        url := "https://gitlab.com/user/project.git"
+        got := scrubUrl(url)
+        want := repoStructure
+
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
 }
