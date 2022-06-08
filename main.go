ab5d8e5cbea48801145fa8f928e805cbdeeaa914
John van Hienen <jvanhienen@gmail.com>
2022-06-08 22:37:24 +0200

    Added version flag and positional argument for url. Also improved cleaning of  cloning artifacts

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "os"
     "os/exec"
+    "runtime"
     "strings"
+)
+
+var (
+    version    = "v0.1.0"
+    goVersion  = runtime.Version()
+    versionStr = fmt.Sprintf("Smartclone version %v\n%v", version, goVersion)
 )
 
 type Repo struct {
@@ -21,9 +29,17 @@ func (c *context) fillDefaults() {
 }
 
 func main() {
+    versionFlag := flag.Bool("v", false, "Displays the version number of Smartclone and Go.")
+    flag.Parse()
+
+    if *versionFlag {
+        fmt.Println(versionStr)
+        os.Exit(0)
+    }
+    url := flag.Arg(0)
+
     ctx := context{}
     ctx.fillDefaults()
-    url := "git@gitlab.com:jvanhienen/dotfiles.git"
     repo := scrubUrl(url)
     clonePath, err := createDirPath(repo, ctx)
     if err != nil {
@@ -32,6 +48,7 @@ func main() {
     err = cloneRepo(clonePath, url)
     if err != nil {
         fmt.Println(err)
+        cleanupPathArtifacts(clonePath)
     }
 
 }
@@ -70,14 +87,17 @@ func createDirPath(r Repo, ctx context) (path string, err error) {
 
     if _, err := os.Stat(path); !os.IsNotExist(err) {
         pathWithSource := fmt.Sprintf("%s-%s", path, r.source)
-        defaultAnswer := "n"
+        createAltDir := "n"
         fmt.Printf("Path already exists. Do you want to create '%s' instead? y/N\t", pathWithSource)
-        fmt.Scanf("%s", &defaultAnswer)
+        _, err := fmt.Scanf("%s", &createAltDir)
+        if err != nil {
+            return "", err
+        }
 
-        if strings.ToLower(defaultAnswer) == "n" || defaultAnswer == "" {
+        if strings.ToLower(createAltDir) == "n" || createAltDir == "" {
             fmt.Println("Abort cloning..")
             os.Exit(0)
-        } else if strings.ToLower(defaultAnswer) == "y" {
+        } else if strings.ToLower(createAltDir) == "y" {
             path = pathWithSource
         }
     }
@@ -92,8 +112,18 @@ func cloneRepo(clonePath string, url string) (err error) {
     cmd := exec.Command("git", "clone", url, clonePath)
     err = cmd.Run()
     if err != nil {
-        return err
+        return fmt.Errorf("could not clone repository, error message: %s", err)
     }
     fmt.Printf("Cloned repository '%s' to '%s'", url, clonePath)
     return nil
+}
+
+func cleanupPathArtifacts(path string) {
+    if _, err := os.Stat(path); !os.IsNotExist(err) {
+        fmt.Printf("Removing clone artifacts..\n")
+        err := os.RemoveAll(path)
+        if err != nil {
+            return
+        }
+    }
 }
