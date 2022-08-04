673ba941eecf580099c6bd19b18047ea9a41e88d
John van Hienen <jvanhienen@gmail.com>
2022-08-04 21:36:48 +0200

    Removed the domain portion and always include domain in savepath

--- main.go
+++ main.go
@@ -1,129 +1,117 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "os/exec"
-    "runtime"
-    "strings"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 var (
-    version    = "v0.1.0"
-    goVersion  = runtime.Version()
-    versionStr = fmt.Sprintf("Smartclone version %v\n%v", version, goVersion)
+	version    = "v0.2.0"
+	goVersion  = runtime.Version()
+	versionStr = fmt.Sprintf("Smartclone version %v\n%v", version, goVersion)
 )
 
 type Repo struct {
-    url       string
-    gitDomain string
-    savePath  string
-    cloneDir  string
+	url      string
+	savePath string
+	cloneDir string
 }
 
 func (r *Repo) fillDefaults() {
-    r.cloneDir = fmt.Sprintf("%s/git", os.Getenv("HOME"))
+	r.cloneDir = fmt.Sprintf("%s/git", os.Getenv("HOME"))
 }
 
 func main() {
-    versionFlag := flag.Bool("v", false, "Displays the version number of Smartclone and Go.")
-    flag.Parse()
-    if *versionFlag {
-        fmt.Println(versionStr)
-        os.Exit(0)
-    }
+	versionFlag := flag.Bool("v", false, "Displays the version number of Smartclone and Go.")
+	flag.Parse()
+	if *versionFlag {
+		fmt.Println(versionStr)
+		os.Exit(0)
+	}
 
-    repo := Repo{}
-    repo.fillDefaults()
-    repo.url = flag.Arg(0)
+	repo := Repo{}
+	repo.fillDefaults()
+	repo.url = flag.Arg(0)
 
-    repo.gitDomain, repo.savePath = scrubUrl(repo.url)
-    clonePath, err := createDirPath(repo)
-    if err != nil {
-        fmt.Println(err)
-    }
-    err = cloneRepo(clonePath, repo.url)
-    if err != nil {
-        fmt.Errorf("%s", err)
-        err := cleanupPathArtifacts(clonePath)
-        if err != nil {
-            fmt.Errorf("%s", err)
-        }
-    }
+	repo.savePath = scrubUrl(repo.url)
+	clonePath, err := createDirPath(repo)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = cloneRepo(clonePath, repo.url)
+	if err != nil {
+		fmt.Errorf("%s", err)
+		err := cleanupPathArtifacts(clonePath)
+		if err != nil {
+			fmt.Errorf("%s", err)
+		}
+	}
 
 }
 
-func scrubUrl(url string) (gitDomain string, savePath string) {
-    if strings.HasPrefix(url, "git@") {
-        return scrubSshUrl(url)
-    } else if strings.HasPrefix(url, "https://") {
-        return scrubHttpsUrl(url)
-    } else {
-        fmt.Println("Please provide a url that starts with 'git@' or 'https://'")
-        os.Exit(1)
-    }
-    return
+func scrubUrl(url string) (gitDomain string) {
+	if strings.HasPrefix(url, "git@") {
+		return scrubSshUrl(url)
+	} else if strings.HasPrefix(url, "https://") {
+		return scrubHttpsUrl(url)
+	} else {
+		fmt.Println("Please provide a url that starts with 'git@' or 'https://'")
+		os.Exit(1)
+	}
+	return
 }
 
-func scrubSshUrl(originalUrl string) (gitDomain string, savePath string) {
-    splittedUrl := strings.Split(originalUrl, ":")
-    gitDomain = strings.TrimPrefix(splittedUrl[0], "git@")
-    savePath = strings.TrimSuffix(splittedUrl[1], ".git")
-    return gitDomain, savePath
+func scrubSshUrl(originalUrl string) (savePath string) {
+	splittedUrl := strings.Split(originalUrl, ":")
+	gitDomain := strings.TrimPrefix(splittedUrl[0], "git@")
+	subpath := strings.TrimSuffix(splittedUrl[1], ".git")
+	savePath = fmt.Sprintf("%s/%s", gitDomain, subpath)
+	return savePath
 }
 
-func scrubHttpsUrl(originalUrl string) (gitDomain string, savePath string) {
-    gitDomain = strings.Split(originalUrl, "/")[2]
-    prefix := fmt.Sprintf("https://%s/", gitDomain)
-    pathNoPrefix := strings.TrimPrefix(originalUrl, prefix)
-    savePath = strings.TrimSuffix(pathNoPrefix, ".git")
-    return gitDomain, savePath
+func scrubHttpsUrl(originalUrl string) (savePath string) {
+	prefix := "https://"
+	pathNoPrefix := strings.TrimPrefix(originalUrl, prefix)
+	savePath = strings.TrimSuffix(pathNoPrefix, ".git")
+	return savePath
 }
 
 func createDirPath(r Repo) (path string, err error) {
-    path = fmt.Sprintf("%s/%s", r.cloneDir, r.savePath)
+	path = fmt.Sprintf("%s/%s", r.cloneDir, r.savePath)
 
-    if _, err := os.Stat(path); !os.IsNotExist(err) {
-        pathWithSource := fmt.Sprintf("%s-%s", path, r.gitDomain)
-        createAltDir := "n"
-        fmt.Printf("Path already exists. Do you want to create '%s' instead? y/N\t", pathWithSource)
-        _, err := fmt.Scanf("%s", &createAltDir)
-        if err != nil {
-            return "", err
-        }
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		fmt.Println("Path already exists. Aborting clone..")
+		os.Exit(0)
+	}
 
-        if strings.ToLower(createAltDir) == "n" || createAltDir == "" {
-            fmt.Println("Abort cloning..")
-            os.Exit(0)
-        } else if strings.ToLower(createAltDir) == "y" {
-            path = pathWithSource
-        }
-    }
-    err = os.MkdirAll(path, 0755)
-    if err != nil {
-        return "", err
-    }
-    return path, nil
+	err = os.MkdirAll(path, 0755)
+	if err != nil {
+		return "", err
+	}
+	return path, nil
 }
 
 func cloneRepo(clonePath string, url string) (err error) {
-    cmd := exec.Command("git", "clone", url, clonePath)
-    err = cmd.Run()
-    if err != nil {
-        return fmt.Errorf("could not clone repository, error message: %s", err)
-    }
-    fmt.Printf("Cloned repository '%s' to '%s'\n", url, clonePath)
-    return nil
+	cmd := exec.Command("git", "clone", url, clonePath)
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("could not clone repository, error message: %s", err)
+	}
+	fmt.Printf("Cloned repository '%s' to '%s'\n", url, clonePath)
+	return nil
 }
 
 func cleanupPathArtifacts(path string) (err error) {
-    if _, err := os.Stat(path); !os.IsNotExist(err) {
-        fmt.Printf("Removing clone artifacts..\n")
-        err := os.RemoveAll(path)
-        if err != nil {
-            return err
-        }
-    }
-    return
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		fmt.Printf("Removing clone artifacts..\n")
+		err := os.RemoveAll(path)
+		if err != nil {
+			return err
+		}
+	}
+	return
 }
--- main_test.go
+++ main_test.go
@@ -1,106 +1,104 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "os"
-    "testing"
+	"errors"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func TestScrubUrl(t *testing.T) {
-    t.Run("ssh git url", func(t *testing.T) {
-        url := "git@gitlab.com:org/project.git"
-        desiredRepoStructure := Repo{
-            url:       url,
-            gitDomain: "gitlab.com",
-            savePath:  "org/project",
-        }
+	t.Run("ssh git url", func(t *testing.T) {
+		url := "git@gitlab.com:org/project.git"
+		desiredRepoStructure := Repo{
+			url:      url,
+			savePath: "gitlab.com/org/project",
+		}
 
-        repo := Repo{}
-        repo.url = url
-        repo.gitDomain, repo.savePath = scrubUrl(repo.url)
+		repo := Repo{}
+		repo.url = url
+		repo.savePath = scrubUrl(repo.url)
 
-        got := repo
-        want := desiredRepoStructure
+		got := repo
+		want := desiredRepoStructure
 
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-    })
-    t.Run("https git url", func(t *testing.T) {
-        url := "https://gitlab.com/org/project.git"
-        desiredRepoStructure := Repo{
-            url:       url,
-            gitDomain: "gitlab.com",
-            savePath:  "org/project",
-        }
+	})
+	t.Run("https git url", func(t *testing.T) {
+		url := "https://gitlab.com/org/project.git"
+		desiredRepoStructure := Repo{
+			url:      url,
+			savePath: "gitlab.com/org/project",
+		}
 
-        repo := Repo{}
-        repo.url = url
-        repo.gitDomain, repo.savePath = scrubUrl(repo.url)
+		repo := Repo{}
+		repo.url = url
+		repo.savePath = scrubUrl(repo.url)
 
-        got := repo
-        want := desiredRepoStructure
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
-    })
+		got := repo
+		want := desiredRepoStructure
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
 
 func TestCleanupPathArtifacts(t *testing.T) {
-    t.Run("Cleanup artifacts", func(t *testing.T) {
-        path := t.TempDir()
-        err := cleanupPathArtifacts(path)
-        if err != nil {
-            t.Errorf("failed with error: %s", err)
-        }
-    })
+	t.Run("Cleanup artifacts", func(t *testing.T) {
+		path := t.TempDir()
+		err := cleanupPathArtifacts(path)
+		if err != nil {
+			t.Errorf("failed with error: %s", err)
+		}
+	})
 }
 
 func TestCreateDirPath(t *testing.T) {
-    t.Run("Clone https repository", func(t *testing.T) {
-        tmpdir := t.TempDir()
-        repo := Repo{}
-        repo.fillDefaults()
-        repo.url = "https://github.com/github/gitignore"
-        repo.cloneDir = tmpdir
-        repo.gitDomain, repo.savePath = scrubUrl(repo.url)
-        clonePath, err := createDirPath(repo)
-        defer cleanupPathArtifacts(repo.cloneDir)
+	t.Run("Clone https repository", func(t *testing.T) {
+		tmpdir := t.TempDir()
+		repo := Repo{}
+		repo.fillDefaults()
+		repo.url = "https://github.com/github/gitignore"
+		repo.cloneDir = tmpdir
+		repo.savePath = scrubUrl(repo.url)
+		clonePath, err := createDirPath(repo)
+		defer cleanupPathArtifacts(repo.cloneDir)
 
-        if err != nil {
-            t.Errorf("failed with error: %s", err)
-        }
+		if err != nil {
+			t.Errorf("failed with error: %s", err)
+		}
 
-        got := clonePath
-        want := fmt.Sprintf("%s/github/gitignore", tmpdir)
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+		got := clonePath
+		want := fmt.Sprintf("%s/github.com/github/gitignore", tmpdir)
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-    })
+	})
 
 }
 
 func TestCloneRepo(t *testing.T) {
-    t.Run("Clone https repository", func(t *testing.T) {
-        tmpdir := t.TempDir()
-        repo := Repo{}
-        repo.fillDefaults()
-        repo.url = "https://github.com/github/gitignore"
-        repo.cloneDir = tmpdir
-        repo.gitDomain, repo.savePath = scrubUrl(repo.url)
-        clonePath, _ := createDirPath(repo)
-        defer cleanupPathArtifacts(repo.cloneDir)
-        fmt.Println()
-        err := cloneRepo(clonePath, repo.url)
-        if err != nil {
-            t.Errorf("failed with error: %s", err)
-        }
-        gotPath := fmt.Sprintf("%s/github/gitignore/Go.gitignore", tmpdir)
-        if _, err := os.Stat(gotPath); errors.Is(err, os.ErrNotExist) {
-            t.Errorf("File does not exist. Detailed error: %s", err)
-        }
-    })
+	t.Run("Clone https repository", func(t *testing.T) {
+		tmpdir := t.TempDir()
+		repo := Repo{}
+		repo.fillDefaults()
+		repo.url = "https://github.com/github/gitignore"
+		repo.cloneDir = tmpdir
+		repo.savePath = scrubUrl(repo.url)
+		clonePath, _ := createDirPath(repo)
+		defer cleanupPathArtifacts(repo.cloneDir)
+		fmt.Println()
+		err := cloneRepo(clonePath, repo.url)
+		if err != nil {
+			t.Errorf("failed with error: %s", err)
+		}
+		gotPath := fmt.Sprintf("%s/github.com/github/gitignore/Go.gitignore", tmpdir)
+		if _, err := os.Stat(gotPath); errors.Is(err, os.ErrNotExist) {
+			t.Errorf("File does not exist. Detailed error: %s", err)
+		}
+	})
 }
